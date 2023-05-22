d6e99dd4e30408bd1233382cea1da31aaaf05f69
aokealy <aokealy@gmail.com>
2023-05-22 10:39:47 +0000

    create a delete function and link it

--- base/views.py
+++ base/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Task
 from django.urls import reverse_lazy
 
@@ -24,6 +24,12 @@ class TaskCreate(CreateView):
 class TaskUpdate(UpdateView):
     model = Task
     fields = '__all__'
-    success_url = reverse_lazy('tasks')    
+    success_url = reverse_lazy('tasks')   
+
+# delete a task 
+class DeleteView(DeleteView):
+    model = Task 
+    context_object_name = 'task'   
+    success_url = reverse_lazy('tasks') 
 
    
