4bdbe6c373964a2cdbaae092f3e68b1f05bc927f
aokealy <aokealy@gmail.com>
2023-05-22 12:09:00 +0000

    add login and logout functionallity

--- base/views.py
+++ base/views.py
@@ -5,6 +5,17 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Task
 from django.urls import reverse_lazy
 
+from django.contrib.auth.views import LoginView
+
+# login functionality
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
 class TaskList(ListView):
     model = Task
     context_object_name = 'tasks'
