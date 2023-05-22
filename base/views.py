0fe61a4466f6af33e4f36bffa2583c0f63d4f301
aokealy <aokealy@gmail.com>
2023-05-22 10:24:15 +0000

    add create view to be able to create tasks

--- base/views.py
+++ base/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from .models import Task
+from django.urls import reverse_lazy
 
 class TaskList(ListView):
     model = Task
@@ -11,4 +13,11 @@ class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task.html'
+
+# Create a task
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
    
